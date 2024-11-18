ab618663e9190afd2741a88ee2fa54627e23efaf
David Fraser <davidf@inconceivable.co.za>
2024-11-18 13:18:23 +0000

    Update this to do some automatic fetching and merging, if possible
    (cherry picked from commit 77897a23d339ceda54619767ee5210be45cc19aa)

--- step30-build-social-app.sh
+++ step30-build-social-app.sh
@@ -8,11 +8,57 @@ set -o allexport
 . "$params_file"
 set +o allexport
 
-show_heading "Fetching source code" "from the different repositories"
+repoDirs="`make echo | grep ^repoDirs: | sed 's/^repoDirs: //'`"
+missingRepos="`for repoDir in ${repoDirs}; do [ -d "$repoDir" ] || echo $repoDir ; done`"
+show_heading "Cloning source code" "from the different repositories"
 make cloneAll
 
-show_heading "Making work branches" "for the different repositories"
-make createWorkBranch
+show_heading "Fetching latest" "from the different repositories"
+for repoDir in $repoDirs
+  do
+    (
+      cd $repoDir
+      echo "$blue_color$clear_bold`basename ${repoDir}`$reset_color"
+      git fetch --all
+      git diff --exit-code --stat || { show_error "Uncommitted changes in `basename $repoDir`:" "inspect $repoDir and adjust as necessary" ; exit 1 ; }
+      current_branch="`git rev-parse --abbrev-ref HEAD`"
+      if [[ "$current_branch" == "main" ]]
+        then
+          echo already on main branch - fast forwarding
+          git merge origin/main
+      elif [[ "$current_branch" == "work" || "$current_branch" == "dockerbuild" ]]
+        then
+          echo switching from branch $current_branch to main
+          git checkout main
+          git merge origin/main
+        else
+          show_error "Unexpected branch in `basename "$repoDir"`:" "$current_branch - inspect $repoDir and adjust as necessary"
+          exit 1
+        fi
+    ) || { echo aha; exit 1; }
+  done
+
+if [ "$missingRepos" != "" ]
+  then
+    show_heading "Making work branches" "for the different repositories"
+    make createWorkBranch
+  else
+    show_heading "Switching to work branches" "for the different repositories and merging main"
+    make checkout2work
+    for repoDir in $repoDirs
+      do
+        (
+          cd $repoDir
+          current_branch="`git rev-parse --abbrev-ref HEAD`"
+          if [ "$current_branch" != "work" ]
+            then
+              git checkout -b work main
+            fi
+          git merge main
+        ) || { show_error "Error merging main into `basename $repoDir`:" "inspect $repoDir and ajdust as necessary" ; exit 1 ; }
+      done
+  fi
+
 
 show_heading "Patching social-app" "to change the branding"
 [ "$REBRANDING_NAME" == "" ] && { show_error "Brand name undefined:" "please set REBRANDING_NAME in $params_file" ; exit 1 ; }
