5cc668eb19dc9ad8a7acb65a48bf3bc674607798
David Fraser <davidf@inconceivable.co.za>
2024-11-22 12:30:17 +0000

    Extract script that handles doing the automatic branching To some extent, use this rather than make commands
    (cherry picked from commit f90c1a127f806e4119f08d5eed190e04854b39fc)

---
+++ autobranch.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# This allows the automated creation of branches, with easier controls over handling common scenarios
+
+script_path="`realpath "$0"`"
+script_dir="`dirname "$script_path"`"
+. "$script_dir/utils.sh"
+
+function usage {
+  echo ""
+  echo "Usage: $0 [--clobber] target_branch upstream_branch [allowed_starting_branch ...]"
+  echo ""
+  echo "Switches to target_branch, based on latest upstream_branch"
+  echo "Will not do this and exit with an error code if:"
+  echo " - there are uncommitted changes"
+  echo " - not on target_branch or one of the existing allowed_starting_branches"
+  echo ""
+  echo "Options:"
+  echo " --clobber|-C will overwrite the target_branch rather than merging in changes from upstream_branch, if it exists"
+  echo ""
+}
+
+if [[ "$#" -lt 2 ]]
+  then
+    usage
+    exit 1
+  fi
+
+do_clobber=
+case "$1" in
+  -C|--clobber)
+    do_clobber=true
+    shift
+esac
+
+target_branch="$1"
+upstream_branch="$2"
+shift
+shift
+allowed_starting_branches="$@"
+repoDir="`pwd`"
+repoName="`basename "$repoDir"`"
+
+git fetch --all
+git diff --exit-code --stat || { show_error "Uncommitted changes in $repoName:" "inspect $repoDir and adjust as necessary" ; exit 1 ; }
+current_branch="`git rev-parse --abbrev-ref HEAD`"
+
+if [[ "$current_branch" == "$target_branch" ]]
+  then
+    echo already on $target_branch branch - merging upstream changes from $upstream_branch
+elif [[ "$current_branch" == "`basename $upstream_branch`" ]]
+  then
+    echo currently on $current_branch branch - switching to $target_branch and merging upstream changes from $upstream_branch
+elif [[ "$allowed_starting_branches" != "" ]]
+  then
+    is_allowed=
+    for allowed_starting_branch in $allowed_starting_branches
+      do
+        if [[ "$current_branch" == "$allowed_starting_branch" ]]
+          then
+            is_allowed=true
+          fi
+      done
+    if [[ "$is_allowed" != "true" ]]
+      then
+        show_error "unknown branch $current_branch" "in $repoName is not one of the allowed starting branches [$allowed_starting_branches], aborting autobranch"
+        exit 1
+      fi
+    echo currently on $current_branch branch - switching to $target_branch and merging upstream changes from $upstream_branch
+fi
+
+if [ -n "$(git branch --list $target_branch)" ]
+  then
+    if [[ "$do_clobber" == true ]]
+      then
+        show_warning "Overwriting $repoName branch $target_branch" "based on $upstream_branch; was at commit `git log --oneline -1 $target_branch`"
+        git checkout -B $target_branch $upstream_branch
+      else
+        git checkout $target_branch
+        git merge $upstream_branch
+    fi
+else
+  git checkout -b $target_branch $upstream_branch
+fi
+
+
--- step30-build-branded.sh
+++ step30-build-branded.sh
@@ -13,51 +13,15 @@ missingRepos="`for repoDir in ${repoDirs}; do [ -d "$repoDir" ] || echo $repoDir
 show_heading "Cloning source code" "from the different repositories"
 make cloneAll
 
-show_heading "Fetching latest" "from the different repositories"
+show_heading "Auto-creating latest work branch" "from the different repositories"
 for repoDir in $repoDirs
   do
     (
       cd $repoDir
       echo "$blue_color$clear_bold`basename ${repoDir}`$reset_color"
-      git fetch --all
-      git diff --exit-code --stat || { show_error "Uncommitted changes in `basename $repoDir`:" "inspect $repoDir and adjust as necessary" ; exit 1 ; }
-      current_branch="`git rev-parse --abbrev-ref HEAD`"
-      if [[ "$current_branch" == "main" ]]
-        then
-          echo already on main branch - fast forwarding
-          git merge origin/main
-      elif [[ "$current_branch" == "work" || "$current_branch" == "dockerbuild" || "$current_branch" == "local-rebranding-$REBRANDING_NAME" ]]
-        then
-          echo switching from branch $current_branch to main
-          git checkout main
-          git merge origin/main
-        else
-          show_error "Unexpected branch in `basename "$repoDir"`:" "$current_branch - inspect $repoDir and adjust as necessary"
-          exit 1
-        fi
-    ) || { echo aha; exit 1; }
+      $script_dir/autobranch.sh work origin/main dockerbuild local-rebranding-$REBRANDING_NAME
+    ) || { show_error "Error updating to latest branch:" "inspect $repoDir and adjust as necessary" ; exit 1 ; }
   done
-
-if [ "$missingRepos" != "" ]
-  then
-    show_heading "Making work branches" "for the different repositories"
-    make createWorkBranch
-  else
-    show_heading "Switching to work branches" "for the different repositories and merging main"
-    make checkout2work
-    for repoDir in $repoDirs
-      do
-        (
-          cd $repoDir
-          current_branch="`git rev-parse --abbrev-ref HEAD`"
-          if [ "$current_branch" != "work" ]
-            then
-              git checkout -b work main
-            fi
-          git merge main
-        ) || { show_error "Error merging main into `basename $repoDir`:" "inspect $repoDir and ajdust as necessary" ; exit 1 ; }
-      done
-  fi
 
 
 if [ "$REBRANDING_DISABLED" == "true" ]
@@ -75,11 +39,7 @@ elif [ "$REBRANDING_SCRIPT" == "" ]
     [ "$REBRANDING_NAME" == "" ] && { show_error "Brand name undefined:" "please set REBRANDING_NAME in $params_file" ; exit 1 ; }
     (
       cd "$script_dir/repos/social-app"
-      if [ -n "$(git branch --list local-rebranding-$REBRANDING_NAME)" ]
-        then
-          show_warning "Overriding social-app branch" "local-rebranding-$REBRANDING_NAME; was at commit `git log --oneline -1 local-rebranding-$REBRANDING_NAME`"
-        fi
-      git checkout -B local-rebranding-$REBRANDING_NAME work
+      $script_dir/autobranch.sh -C local-rebranding-$REBRANDING_NAME work dockerbuild
       "$REBRANDING_SCRIPT_ABS" $REBRANDING_NAME
       git commit -a -m "Automatic rebranding to $REBRANDING_NAME"
     )
