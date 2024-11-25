c20e084e787225675900f954c77e8153a90251b0
futurGH <me@futuristick.ca>
2024-11-25 10:43:56 -0500

    Use root domain for both pds and social-app

--- step50-test-create-account.sh
+++ step50-test-create-account.sh
@@ -9,10 +9,10 @@ set -o allexport
 set +o allexport
 
 export u=firstever
-test_account_handle=${u}.pds.${DOMAIN}
+test_account_handle=${u}.${DOMAIN}
 test_account_password=`pwgen 16`
 test_account_email=${u}@example.com
-show_heading "Test creating account" $handle with password $test_account_password and email $test_account_email 
+show_heading "Test creating account" $handle with password $test_account_password and email $test_account_email
 make api_CreateAccount handle=$test_account_handle password=$test_account_password email=$test_account_email resp=./data/accounts/${u}.secrets
 
 
