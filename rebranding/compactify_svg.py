b63f647c4532a30909fb6e43c4aa23c8b03f1848
David Fraser <davidf@inconceivable.co.za>
2024-11-27 10:25:19 +0000

    Don't let compactify remove gradients that are actually used

--- rebranding/compactify_svg.py
+++ rebranding/compactify_svg.py
@@ -3,6 +3,7 @@
 import xml.etree.ElementTree as etree
 import logging
 import sys
+import re
 
 NS = {
   'svg': '{http://www.w3.org/2000/svg}',
@@ -10,6 +11,8 @@ NS = {
 }
 
 remove_font_styles = {'font-weight', 'font-size', 'font-family', '-inkscape-font-specification', 'line-height'}
+
+style_url_re = re.compile('url[(]#([^)]*)[)]')
 
 def register_all_namespaces(filename):
     namespaces = dict([node for _, node in etree.iterparse(filename, events=['start-ns'])])
@@ -26,12 +29,28 @@ def compactify(input_filename, output_filename):
     svg_file = etree.parse(input_filename)
     logging.info("Parsed %s", svg_file)
     root = svg_file.getroot()
+    used_ids = []
+    for object_with_style in root.findall(f'.//{NS["svg"]}*[@style]'):
+        object_style = object_with_style.attrib.get('style')
+        used_ids.extend(style_url_re.findall(object_style))
+    for gradient in root.findall(f'.//{NS["svg"]}linearGradient'):
+        gradient_id = gradient.attrib['id']
+        if gradient_id in used_ids:
+            href = gradient.attrib.get(f'{NS["xlink"]}href')
+            if href and href.startswith('#'):
+                href_id = href[1:]
+                if href_id not in used_ids:
+                    used_ids.append(href_id)
     for gradient_parent in root.findall(f'.//{NS["svg"]}linearGradient/..'):
         removals = []
         for child in gradient_parent:
             if child.tag == f'{NS["svg"]}linearGradient':
-                print(f"removing {child.tag} {child.attrib['id']}")
-                removals.append(child)
+                child_id = child.attrib['id']
+                if child_id in used_ids:
+                    print(f"keeping {child.tag} {child_id}; in use")
+                else:
+                    print(f"removing {child.tag} {child_id}")
+                    removals.append(child)
         for child in removals:
             gradient_parent.remove(child)
     for defs_parent in root.findall(f'.//{NS["svg"]}defs/..'):
