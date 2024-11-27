435e593b9ab89307a524f127887af28de0f9693d
David Fraser <davidf@inconceivable.co.za>
2024-11-27 20:13:06 +0000

    Actually check web responses and fail if it doesn't work

--- step20-test-web-hosting.sh
+++ step20-test-web-hosting.sh
@@ -10,24 +10,39 @@ set +o allexport
 
 show_heading "Starting test web containers" "to check caddy configuration"
 
-make docker-start-nowatch f=./docker-compose-debug-caddy.yaml services=
+# make docker-start-nowatch f=./docker-compose-debug-caddy.yaml services=
+
+show_heading "Artificial delay" "to allow letsencrypt to work"
+sleep 10
+show_heading "Wait for startup" "of test web containers"
+wait_for_container caddy
 
 show_heading "Checking test web containers"
 
 # test HTTPS and WSS with your docker environment
-curl -L "https://test-wss.${DOMAIN}/"
-echo test successful | websocat "wss://test-wss.${DOMAIN}/ws"
+failures=
+curl -L "https://test-wss.${DOMAIN}/" || { show_warning "Error testing https" "to test-wss.${DOMAIN}" ; failures="$failures https://test-wss" ; }
+echo test successful | websocat "wss://test-wss.${DOMAIN}/ws" || { show_warning "Error testing wss" "to test-wss.${DOMAIN}" ; failures="$failures wss://test-wss" ; }
 
 # test reverse proxy mapping if it works as expected for bluesky
 #  those should be redirect to PDS
-curl -L "https://pds.${DOMAIN}/xrpc/any-request" | jq
-curl -L "https://random-`pwgen 6`.pds.${DOMAIN}/xrpc/any-request" | jq
+{ curl -L "https://pds.${DOMAIN}/xrpc/any-request" | jq ; } || { show_warning "Error testing xrpc" "to pds.${DOMAIN}" ; failures="$failures https://pds/xrpc" ; }
+random_name=`pwgen 6`
+{ curl -L "https://random-${random_name}.pds.${DOMAIN}/xrpc/any-request" | jq ; } || { show_warning "Error testing xrpc" "to random-${random_name}.pds.${DOMAIN}" ; failures="$failures https://random/xrpc" ; }
 
 #  those should be redirect to social-app
-curl -L "https://pds.${DOMAIN}/others" | jq
-curl -L "https://random-`pwgen 6`.pds.${DOMAIN}/others" | jq
+{ curl -L "https://pds.${DOMAIN}/others" | jq ; } || { show_warning "Error testing https" "to pds.${DOMAIN}/others" ; failures="$failures https://pds/others" ; }
+{ curl -L "https://random-${random_name}.pds.${DOMAIN}/others" | jq ; } || { show_warning "Error testing https" "to random-${random_name}.pds.${DOMAIN}" ; failures="$failures https://random/others" ; }
 
-# TODO: actually check these responses
+if [ "$failures" = "" ]
+  then
+    show_info "Tests passed"
+  else
+    show_error "Tests failed:" "$failures"
+    show_info "Debug before proceeding:" "test containers left running"
+    exit 1
+  fi
+
 
 show_heading "Stopping test web containers" "without persisting data"
 
