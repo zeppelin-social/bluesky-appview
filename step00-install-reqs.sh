d8482647e131bf6650e33dd0dca6f4b46c98c74a
David Fraser <davidf@inconceivable.co.za>
2024-11-27 20:12:26 +0000

    jq is actually used by the testing code

--- step00-install-reqs.sh
+++ step00-install-reqs.sh
@@ -5,14 +5,14 @@ script_dir="`dirname "$script_path"`"
 . "$script_dir/utils.sh"
 
 show_heading "Setting up apt packages" that are requirements for building running and testing these docker images
-if dpkg-query -l make pwgen
+if dpkg-query -l make pwgen jq
   then
     show_info "No install required:" all packages already installed
   else
     sudo apt update
     # make is used to run setup scripts etc
     # pwgen is used to generate new securish passwords
-    sudo apt install -y make pwgen
+    sudo apt install -y make pwgen jq
   fi
 
 show_heading "Setting up websocat" directly from executable download, in /usr/local/bin
