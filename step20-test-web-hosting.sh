23f34f2fb328b3a9fc5b37f03d753630bef10e93
David Fraser <davidf@inconceivable.co.za>
2024-11-30 07:09:16 +0000

    Evaluate the results of the tests, and fail accordingly
    Start scripting using the certificates retrieved from the test web
    containers to test self-signed mode
    (This is preferential to installing them on the server, but it's working
    consistently yet)

--- step20-test-web-hosting.sh
+++ step20-test-web-hosting.sh
@@ -10,12 +10,20 @@ set +o allexport
 
 show_heading "Starting test web containers" "to check caddy configuration"
 
-make docker-start-nowatch f=./docker-compose-debug-caddy.yaml services=
+show_info "Stopping test web containers" "in case any are still running"
+make docker-stop f=./docker-compose-debug-caddy.yaml services= || { show_warning "Error stopping services:" "please examine and fix" ; exit 1 ; }
+show_info "Starting test web containers"
+make docker-start-nowatch f=./docker-compose-debug-caddy.yaml services= || { show_error "Error starting containers:" "please examine and fix" ; exit 1 ; }
 
 show_heading "Wait for startup" "of test web containers"
-wait_for_container caddy
-if [ "$EMAIL4CERT" != "internal" ]
+wait_for_container caddy || { show_warning "Error waiting for caddy:" "it may not have started correctly" ; exit 1 ; }
+
+show_info "Current value of EMAIL4CERTS:" "$EMAIL4CERTS"
+if [ "$EMAIL4CERTS" == "internal" ]
   then
+    CURL_ARGS="--cacert `pwd`/certs/root.crt --cacert `pwd`/certs/intermediate.crt"
+    show_info "Using CA Cert" "for tests: $CURL_ARGS"
+  else
     show_heading "Artificial delay" "to allow letsencrypt to work"
     sleep 20
   fi
@@ -24,18 +32,18 @@ show_heading "Checking test web containers"
 
 # test HTTPS and WSS with your docker environment
 failures=
-curl -L "https://test-wss.${DOMAIN}/" || { show_warning "Error testing https" "to test-wss.${DOMAIN}" ; failures="$failures https://test-wss" ; }
+curl ${CURL_ARGS} -L "https://test-wss.${DOMAIN}/" || { show_warning "Error testing https" "to test-wss.${DOMAIN}" ; failures="$failures https://test-wss" ; }
 echo test successful | websocat "wss://test-wss.${DOMAIN}/ws" || { show_warning "Error testing wss" "to test-wss.${DOMAIN}" ; failures="$failures wss://test-wss" ; }
 
 # test reverse proxy mapping if it works as expected for bluesky
 #  those should be redirect to PDS
-{ curl -L "https://pds.${DOMAIN}/xrpc/any-request" | jq ; } || { show_warning "Error testing xrpc" "to pds.${DOMAIN}" ; failures="$failures https://pds/xrpc" ; }
+{ curl ${CURL_ARGS} -L "https://pds.${DOMAIN}/xrpc/any-request" | jq ; } || { show_warning "Error testing xrpc" "to pds.${DOMAIN}" ; failures="$failures https://pds/xrpc" ; }
 random_name=`pwgen 6`
-{ curl -L "https://random-${random_name}.pds.${DOMAIN}/xrpc/any-request" | jq ; } || { show_warning "Error testing xrpc" "to random-${random_name}.pds.${DOMAIN}" ; failures="$failures https://random/xrpc" ; }
+{ curl ${CURL_ARGS} -L "https://random-${random_name}.pds.${DOMAIN}/xrpc/any-request" | jq ; } || { show_warning "Error testing xrpc" "to random-${random_name}.pds.${DOMAIN}" ; failures="$failures https://random/xrpc" ; }
 
 #  those should be redirect to social-app
-{ curl -L "https://pds.${DOMAIN}/others" | jq ; } || { show_warning "Error testing https" "to pds.${DOMAIN}/others" ; failures="$failures https://pds/others" ; }
-{ curl -L "https://random-${random_name}.pds.${DOMAIN}/others" | jq ; } || { show_warning "Error testing https" "to random-${random_name}.pds.${DOMAIN}" ; failures="$failures https://random/others" ; }
+{ curl ${CURL_ARGS} -L "https://pds.${DOMAIN}/others" | jq ; } || { show_warning "Error testing https" "to pds.${DOMAIN}/others" ; failures="$failures https://pds/others" ; }
+{ curl ${CURL_ARGS} -L "https://random-${random_name}.pds.${DOMAIN}/others" | jq ; } || { show_warning "Error testing https" "to random-${random_name}.pds.${DOMAIN}" ; failures="$failures https://random/others" ; }
 
 if [ "$failures" = "" ]
   then
@@ -49,5 +57,5 @@ if [ "$failures" = "" ]
 
 show_heading "Stopping test web containers" "without persisting data"
 
-make    docker-stop-with-clean f=./docker-compose-debug-caddy.yaml
+make docker-stop-with-clean f=./docker-compose-debug-caddy.yaml
 
