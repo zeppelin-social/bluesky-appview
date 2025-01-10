c73659f75815c7d756ada133907a4cc134561fde
futurGH <me@futuristick.ca>
2025-01-09 22:09:04 -0500

    Allow specifying service to build

--- step30-build-branded.sh
+++ step30-build-branded.sh
@@ -85,15 +85,24 @@ show_heading "Patching each repository" "with changes required for docker build"
 # NOTE: this op checks out a new branch before applying patch, and stays on the new branch
 make patch-dockerbuild checkoutbranchargs=-B commitargs=--no-verify
 
-for branded_service in $REBRANDED_SERVICES
-  do
-    show_heading "Building $branded_service" "customized for domain $DOMAIN"
-    # 1) build image, customized for domain
-    make build f=./docker-compose-builder.yaml services=$branded_service
-  done
-
+if [ $# -gt 0 ]
+    then
+        for service in $*
+            do
+                show_heading "Building $service" "customized for domain $DOMAIN"
+                # 1) build image, customized for domain
+                make build f=./docker-compose-builder.yaml services=$service
+            done
+    else
+        for branded_service in $REBRANDED_SERVICES
+            do
+                show_heading "Building $branded_service" "customized for domain $DOMAIN"
+                # 1) build image, customized for domain
+                make build f=./docker-compose-builder.yaml services=$branded_service
+            done
+fi
 # show_heading "Building other images" "without domain customization"
 # 2) build images with original
-# make build DOMAIN= f=./docker-compose-builder.yaml 
+# make build DOMAIN= f=./docker-compose-builder.yaml
 
 
