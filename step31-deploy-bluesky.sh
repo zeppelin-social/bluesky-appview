a01d9b0801a04d8c39eeae21038b557aa0ee447a
futurGH <me@futuristick.ca>
2025-05-23 00:51:45 -0400

    Remove social-app wait

--- step31-deploy-bluesky.sh
+++ step31-deploy-bluesky.sh
@@ -21,9 +21,3 @@ make docker-start || { show_error "Required Containers failed:" "Please see erro
 show_heading "Deploy bluesky containers" "(plc, bgs, appview, pds, ozone, ...)"
 make docker-start-bsky || { show_error "BlueSky Containers failed:" "Please see error above" ; exit 1 ; }
 
-show_heading "Wait for startup" "of social app"
-# could also wait for Sbsky ?=pds bgs bsky social-app palomar
-# this requires a health check to be defined on the container
-wait_for_container social-app
-
-
