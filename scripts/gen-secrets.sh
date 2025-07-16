ea6580085a35aadb0fcec362bf252f51c27159e6
futurGH <me@futuristick.ca>
2025-07-16 15:00:09 -0400

    Meet opensearch password requirements

--- scripts/gen-secrets.sh
+++ scripts/gen-secrets.sh
@@ -2,11 +2,12 @@
 
 GENERATE_PASS="openssl ecparam --name secp256k1 --genkey --noout --outform DER | tail --bytes=+8 | head --bytes=32 | xxd --plain --cols 32"
 
+GENERATE_OPENSEARCH_PASS="pwgen -c -n -B -s -1 16 1 | sed 's/$/_/'"
+
 password_names=(
     POSTGRES_PASSWORD
     BSKY_ADMIN_PASSWORDS
     BSKY_SERVICE_SIGNING_KEY
-    OPENSEARCH_PASSWORD
 )
 
 for name in "${password_names[@]}"; do
@@ -17,3 +18,4 @@ echo "Generated secrets:"
 for name in "${password_names[@]}"; do
     echo "${name}=${!name}"
 done
+echo "OPENSEARCH_PASSWORD=$(eval "${GENERATE_OPENSEARCH_PASS}")"
